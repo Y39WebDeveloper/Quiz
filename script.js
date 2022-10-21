28f189777af80a58be20123275f0fd8d762d8d8c
Y39WebDeveloper <y39freelancer@gmail.com>
2022-10-21 17:08:04 +0100

    ...

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let countSpan = document.querySelector(".count span");
 let bullets = document.querySelector(".bullets .spans");
+let questions = document.querySelector(".quiz-area");
+let answers = document.querySelector(".answers-area");
+
+let currentIndex = 0;
 
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
@@ -7,8 +11,10 @@ function getQuestions(){
     myRequest.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200){
             let questionsObject = JSON.parse(this.responseText);
-            let questionsCount = questionsObject.length;
-            createBullets(questionsCount);
+            let qCount = questionsObject.length;
+
+            createBullets(qCount);
+            addQuestionData(questionsObject[currentIndex], qCount);
         }
     }
 
@@ -24,5 +30,30 @@ function createBullets(num){
         let bullet = document.createElement("span");
         if(i == 0){bullet.className = "on";}
         bullets.appendChild(bullet);
+    }
+}
+
+function addQuestionData(obj, count){
+    let question = document.createElement("h2");
+    question.textContent = obj.title;
+    questions.appendChild(question);
+
+    for(let i = 1; i <= 4; i++){
+        let answer = document.createElement("div");
+        answer.className = "answer";
+
+        let radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "questions";
+        radio.id = `answer_${i}`;
+
+        let label = document.createElement('label');
+        label.setAttribute("for", `answer_${i}`);
+        label.textContent = obj[`answer_${i}`];
+
+        answer.appendChild(radio);
+        answer.appendChild(label);
+
+        answers.appendChild(answer);
     }
 }
