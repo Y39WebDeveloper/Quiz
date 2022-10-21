e88e37698570b32357c8b1401dab422c233820ea
Y39WebDeveloper <y39freelancer@gmail.com>
2022-10-21 15:30:55 +0100

    ...

---
+++ script.js
@@ -0,0 +1,27 @@
+let countSpan = document.querySelector(".count span");
+let bullets = document.querySelector(".bullets .spans");
+
+function getQuestions(){
+    let myRequest = new XMLHttpRequest();
+
+    myRequest.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200){
+            let questionsObject = JSON.parse(this.responseText);
+            let questionsCount = questionsObject.length;
+            createBullets(questionsCount);
+        }
+    }
+
+    myRequest.open("GET", "questions.json", true);
+    myRequest.send();
+}
+
+getQuestions();
+
+function createBullets(num){
+    countSpan.innerHTML = num;
+    for(let i = 0; i < num; i++){
+        let bullet = document.createElement("span");
+        bullets.appendChild(bullet);
+    }
+}
